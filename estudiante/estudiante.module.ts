499569f5c65164a61e822e735c8292e1ab1a0c4f
Grupo 4 - Celly, Ortega <jdcelly-2024a@corhuila.edu.co>
2026-05-03 17:23:08 -0500

    Delete estudiante directory

--- estudiante/estudiante.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EstudianteController } from './estudiante.controller';
-import { EstudianteService } from './estudiante.service';
-
-@Module({
-  controllers: [EstudianteController],
-  providers: [EstudianteService]
-})
-export class EstudianteModule {}
