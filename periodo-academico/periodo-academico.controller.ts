a32c3d0a82dd7d80314a2343670f17ea68f6ad27
Grupo 4 - Celly, Ortega <jdcelly-2024a@corhuila.edu.co>
2026-05-03 17:23:58 -0500

    Delete periodo-academico directory

--- periodo-academico/periodo-academico.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('periodo-academico')
-export class PeriodoAcademicoController {}
--- periodo-academico/periodo-academico.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PeriodoAcademicoController } from './periodo-academico.controller';
-import { PeriodoAcademicoService } from './periodo-academico.service';
-
-@Module({
-  controllers: [PeriodoAcademicoController],
-  providers: [PeriodoAcademicoService]
-})
-export class PeriodoAcademicoModule {}
